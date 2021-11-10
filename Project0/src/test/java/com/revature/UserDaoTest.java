a8155383a98cc152c9ff784c68e5ffc814aafff3
tricialynn <plg1003@wildcats.unh.edu>
2021-11-10 00:14:47 -0500

    project progress - added BookDaoTests

--- Project0/src/test/java/com/revature/UserDaoTest.java
+++ Project0/src/test/java/com/revature/UserDaoTest.java
@@ -2,14 +2,15 @@ package com.revature;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.*;
 
+import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.Books;
 import com.revature.models.Bookseller;
 import com.revature.models.Customer;
 import com.revature.models.User;
-import com.revature.repositories.BookArray;
-import com.revature.repositories.UserArray;
 import com.revature.repositories.UserDao;
 
 public class UserDaoTest {
@@ -19,6 +20,7 @@ public class UserDaoTest {
 		
 	}
 	
+	@Order(2)
 	@Test
 	public void addCustomerValid() {
 		User[] expected = {new Bookseller("Tricia", "password") , new Bookseller("Tricia1", "pass", "OWNER"), new Customer("Patricia", "password123"), new Customer("Trisha", "pass")};
@@ -27,4 +29,16 @@ public class UserDaoTest {
 		User[] actual = UserArray.addUser(users, "Trisha", "pass", "CUSTOMER");
 		assertArrayEquals(expected, actual);
 	}
+	
+	
+	List<User> getAllBooksellers();
+	List<User> getAllCustomers();
+	User getUserById(int id, String role);
+	Customer retrieveCustomer(String username);
+	Bookseller retrieveBookseller(String username);
+	boolean findCustomer(String username) throws UserNotFoundException;
+	boolean findBookseller(String username) throws UserNotFoundException;
+	boolean addUser(String fname, String lname, String username, String password, String role);
+	public boolean fireBookseller(String username);
+	boolean updateBookseller(String username);
 }
